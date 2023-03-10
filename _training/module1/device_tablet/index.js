cc6e600a602be7e690bd936a7da1e79396a72ea1
HarikaMunipalle <harika.munipalle@gsa.gov>
2023-03-10 15:46:55 -0500

    Fixed issues

--- _training/module1/device_tablet/index.js
+++ _training/module1/device_tablet/index.js
@@ -1,8 +1,6 @@
 TabletResponsive={"1009":{
 "pageLayer":{"w":1009,"h":662,"bgColor":"#00324b","bgImage":"images/image0000.png","bgSize":"1009px 567px","bgRepeat":"no-repeat"}
 ,
-"text227472":{"x":324,"y":514,"w":360,"h":31,"txtscale":100,"bOffBottom":0}
-,
 "":{"x":354,"y":183,"w":300,"h":200,"bOffBottom":0}
 ,
 "":{"x":354,"y":183,"w":300,"h":200,"bOffBottom":0}
@@ -38,8 +36,6 @@ try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)
 },
 "785":{
 "pageLayer":{"w":785,"h":1000,"bgColor":"#ffffff","bgImage":"images/image0000.png","bgSize":"785px 441px","bgRepeat":"no-repeat"}
-,
-"text227472":{"x":252,"y":514,"w":280,"h":24,"txtscale":100,"bOffBottom":0}
 ,
 "":{"x":275,"y":183,"w":233,"h":156,"bOffBottom":0}
 ,
